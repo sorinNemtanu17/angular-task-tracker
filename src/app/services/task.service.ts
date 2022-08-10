1842eccd07fb136582a92a44f23f09ba87eb4a2b
sorinNemtanu17 <sorinlego@gmail.com>
2022-08-10 13:40:46 +0300

    D5 - working on tasks - added db.json file - adding delete operation

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
-import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiURL = 'http://localhost:5000/tasks';
+  constructor(private http: HttpClient) {}
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiURL);
   }
 }
